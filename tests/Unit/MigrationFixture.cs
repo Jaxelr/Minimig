7a6b3400a2b7ab2d94786810c0f9485b5e5f56da
Jaxel Rojas <jaxelr@gmail.com>
2019-10-05 12:10:25 -0400

    test: add migration fixture

---
+++ tests/Unit/MigrationFixture.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+using Minimig;
+using Xunit;
+using System.IO;
+using System;
+
+namespace MinimigTests.Unit
+{
+    public class MigrationFixture
+    {
+        [Theory]
+        [InlineData("..\\..\\..\\..\\sampleMigrations\\0001 - Add One and Two tables.sql")]
+        public void Check_migration(string filePath)
+        {
+            //Arrange
+            var regex = new Regex("\r\n|\n\r|\n|\r", RegexOptions.Compiled);
+            string filename = Path.GetFileName(filePath);
+
+            //Act
+            var migration = new Migration(filePath, regex);
+
+            //Assert
+            Assert.Equal(migration.Filename, filename);
+            Assert.True(Guid.TryParse(migration.Hash, out _));
+            Assert.True(migration.UseTransaction);
+        }
+
+        [Theory]
+        [InlineData("..\\..\\..\\..\\sampleMigrations\\0003 - Insert Three data.sql")]
+        public void Check_migration_no_transaction(string filePath)
+        {
+            //Arrange
+            var regex = new Regex("\r\n|\n\r|\n|\r", RegexOptions.Compiled);
+            string filename = Path.GetFileName(filePath);
+
+            //Act
+            var migration = new Migration(filePath, regex);
+
+            //Assert
+            Assert.Equal(migration.Filename, filename);
+            Assert.True(Guid.TryParse(migration.Hash, out _));
+            Assert.False(migration.UseTransaction);
+        }
+    }
+}
