b5a00cf6c3a506a79d0c7cb051f4701d3f80efe4
Jaxel Rojas <jaxelr@gmail.com>
2019-10-10 09:42:55 -0400

    test:  adding migrator fixture

---
+++ tests/Unit/MigratorFixture.cs
@@ -0,0 +1,21 @@
+﻿using Minimig;
+using Xunit;
+
+namespace MinimigTests.Unit
+{
+    public class MigratorFixture
+    {
+        [Fact]
+        public void Migrator_get_version()
+        {
+            //Arrange
+            string version = "0.3.0";
+
+            //Act
+            string result = Migrator.GetVersion();
+
+            //Assert
+            Assert.Equal(version, result);
+        }
+    }
+}
