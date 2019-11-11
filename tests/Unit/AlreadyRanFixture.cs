d91f9aa206503263d2e196367a82108b3f39f4d0
Jaxel Rojas <jaxelr@gmail.com>
2019-11-11 11:49:08 -0400

    refactor: created fake migration row and implemented it on  the Already ran fixtures

--- tests/Unit/AlreadyRanFixture.cs
+++ tests/Unit/AlreadyRanFixture.cs
@@ -10,15 +10,7 @@ namespace MinimigTests.Unit
         public void Check_if_migration_row_already_ran_last()
         {
             //Arrange
-            var row = new MigrationRow()
-            {
-                Id = 1,
-                Filename = "c:\\temp\\abc",
-                Duration = 0,
-                Hash = "uniqueKey",
-                ExecutionDate = DateTime.Now
-            };
-
+            var row = new Fakes.FakeMigrationRow();
             var rows = new MigrationRow[1] { row };
             
             //Act
@@ -37,21 +29,12 @@ namespace MinimigTests.Unit
         public void Check_if_migration_row_already_ran_by_filename()
         {
             //Arrange
-            string Filename = "c:\\temp\\abc";
-            var row = new MigrationRow()
-            {
-                Id = 1,
-                Filename = Filename,
-                Duration = 0,
-                Hash = "uniqueKey",
-                ExecutionDate = DateTime.Now
-            };
-
+            var row = new Fakes.FakeMigrationRow();
             var rows = new MigrationRow[1] { row };
 
             //Act
             var ran = new AlreadyRan(rows);
-            var found = ran.ByFilename[Filename];
+            var found = ran.ByFilename[row.Filename];
 
             //Assert
             Assert.Equal(row.Id, found.Id);
@@ -65,21 +48,12 @@ namespace MinimigTests.Unit
         public void Check_if_migration_row_already_ran_by_hash()
         {
             //Arrange
-            string key = "uniqueKey";
-            var row = new MigrationRow()
-            {
-                Id = 1,
-                Filename = "c:\\temp\\abc",
-                Duration = 0,
-                Hash = key,
-                ExecutionDate = DateTime.Now
-            };
-
+            var row = new Fakes.FakeMigrationRow();
             var rows = new MigrationRow[1] { row };
 
             //Act
             var ran = new AlreadyRan(rows);
-            var found = ran.ByHash[key];
+            var found = ran.ByHash[row.Hash];
 
             //Assert
             Assert.Equal(row.Id, found.Id);
@@ -94,15 +68,7 @@ namespace MinimigTests.Unit
         public void Check_migration_row_count()
         {
             //Arrange
-            var row = new MigrationRow()
-            {
-                Id = 1,
-                Filename = "c:\\temp\\abc",
-                Duration = 0,
-                Hash = "uniqueKey",
-                ExecutionDate = DateTime.Now
-            };
-
+            var row = new Fakes.FakeMigrationRow();
             var rows = new MigrationRow[1] { row };
 
             //Act
