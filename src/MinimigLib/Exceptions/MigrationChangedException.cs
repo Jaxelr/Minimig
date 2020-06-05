6445e2e8a35211654b012fc4b38e0e40fc6f1091
Jaxel Rojas <jaxelr@gmail.com>
2020-06-05 07:29:52 -0400

    style: added pragma warning disable to exception

--- src/MinimigLib/Exceptions/MigrationChangedException.cs
+++ src/MinimigLib/Exceptions/MigrationChangedException.cs
@@ -2,7 +2,9 @@
 
 namespace Minimig
 {
+#pragma warning disable RCS1194 // Implement exception constructors.
     public class MigrationChangedException : Exception
+#pragma warning restore RCS1194 // Implement exception constructors.
     {
         internal MigrationChangedException(Migration migration) : base($"{migration.Filename} has been modified since it was run. Use --force to re-run it.")
         {
