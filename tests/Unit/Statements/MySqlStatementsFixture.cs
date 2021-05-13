4935bab44ecdf46139ca56d9ae636c597cb56079
Jaxel Rojas <jaxelr@gmail.com>
2021-05-13 08:13:32 -0400

    tests: added mysql unit test statements

--- tests/Unit/Statements/MySqlStatementsFixture.cs
+++ tests/Unit/Statements/MySqlStatementsFixture.cs
@@ -1,20 +1,20 @@
-using System;
+﻿using System;
 using Minimig;
 using Xunit;
 
 namespace MinimigTests.Unit
 {
-    public class PostgreSqlStatementsFixture
+    public class MySqlStatementsFixture
     {
         [Fact]
-        public void PostgreSql_statements_insert()
+        public void MySql_statements_insert()
         {
             //Arrange
             const string table = "mymigration";
             const string expected = "INSERT";
 
             //Act
-            var s = new PostgreSqlStatements(table);
+            var s = new MySqlStatements(table);
 
             //Assert
             Assert.Contains(expected, s.InsertMigration, StringComparison.InvariantCultureIgnoreCase);
@@ -22,7 +22,7 @@ namespace MinimigTests.Unit
         }
 
         [Fact]
-        public void PostgreSql_statements_insert_with_schema()
+        public void MySql_statements_insert_with_schema()
         {
             //Arrange
             const string table = "mymigration";
@@ -30,7 +30,7 @@ namespace MinimigTests.Unit
             const string expected = "INSERT";
 
             //Act
-            var s = new PostgreSqlStatements(table, schema);
+            var s = new MySqlStatements(table, schema);
 
             //Assert
             Assert.Contains(expected, s.InsertMigration, StringComparison.InvariantCultureIgnoreCase);
@@ -39,14 +39,14 @@ namespace MinimigTests.Unit
         }
 
         [Fact]
-        public void PostgreSql_statements_update()
+        public void MySql_statements_update()
         {
             //Arrange
             const string table = "mymigration";
             const string expected = "UPDATE";
 
             //Act
-            var s = new PostgreSqlStatements(table);
+            var s = new MySqlStatements(table);
 
             //Assert
             Assert.Contains(expected, s.UpdateMigrationHash, StringComparison.InvariantCultureIgnoreCase);
@@ -56,7 +56,7 @@ namespace MinimigTests.Unit
         }
 
         [Fact]
-        public void PostgreSql_statements_update_with_schema()
+        public void MySql_statements_update_with_schema()
         {
             //Arrange
             const string table = "mymigration";
@@ -64,7 +64,7 @@ namespace MinimigTests.Unit
             const string expected = "UPDATE";
 
             //Act
-            var s = new PostgreSqlStatements(table, schema);
+            var s = new MySqlStatements(table, schema);
 
             //Assert
             Assert.Contains(expected, s.UpdateMigrationHash, StringComparison.InvariantCultureIgnoreCase);
@@ -76,14 +76,14 @@ namespace MinimigTests.Unit
         }
 
         [Fact]
-        public void PostgreSql_statements_migration_exists()
+        public void MySql_statements_migration_exists()
         {
             //Arrange
             const string table = "mymigration";
             const string expected = "SELECT";
 
             //Act
-            var s = new PostgreSqlStatements(table);
+            var s = new MySqlStatements(table);
 
             //Assert
             Assert.Contains(expected, s.DoesMigrationsTableExist, StringComparison.InvariantCultureIgnoreCase);
@@ -93,7 +93,7 @@ namespace MinimigTests.Unit
         }
 
         [Fact]
-        public void PostgreSql_statements__migration_exists_with_schema()
+        public void MySql_statements__migration_exists_with_schema()
         {
             //Arrange
             const string table = "mymigration";
@@ -101,7 +101,7 @@ namespace MinimigTests.Unit
             const string expected = "SELECT";
 
             //Act
-            var s = new PostgreSqlStatements(table, schema);
+            var s = new MySqlStatements(table, schema);
 
             //Assert
             Assert.Contains(expected, s.DoesMigrationsTableExist, StringComparison.InvariantCultureIgnoreCase);
@@ -113,14 +113,14 @@ namespace MinimigTests.Unit
         }
 
         [Fact]
-        public void PostgreSql_statements_create_migration()
+        public void MySql_statements_create_migration()
         {
             //Arrange
             const string table = "mymigration";
             const string expected = "CREATE";
 
             //Act
-            var s = new PostgreSqlStatements(table);
+            var s = new MySqlStatements(table);
 
             //Assert
             Assert.Contains(expected, s.CreateMigrationsTable, StringComparison.InvariantCultureIgnoreCase);
@@ -128,7 +128,7 @@ namespace MinimigTests.Unit
         }
 
         [Fact]
-        public void PostgreSql_statements_create_migration_with_schema()
+        public void MySql_statements_create_migration_with_schema()
         {
             //Arrange
             const string table = "mymigration";
@@ -136,7 +136,7 @@ namespace MinimigTests.Unit
             const string expected = "CREATE";
 
             //Act
-            var s = new PostgreSqlStatements(table, schema);
+            var s = new MySqlStatements(table, schema);
 
             //Assert
             Assert.Contains(expected, s.CreateMigrationsTable, StringComparison.InvariantCultureIgnoreCase);
@@ -145,14 +145,14 @@ namespace MinimigTests.Unit
         }
 
         [Fact]
-        public void PostgreSql_statements_drop_migration()
+        public void MySql_statements_drop_migration()
         {
             //Arrange
             const string table = "mymigration";
             const string expected = "DROP";
 
             //Act
-            var s = new PostgreSqlStatements(table);
+            var s = new MySqlStatements(table);
 
             //Assert
             Assert.Contains(expected, s.DropMigrationsTable, StringComparison.InvariantCultureIgnoreCase);
@@ -160,7 +160,7 @@ namespace MinimigTests.Unit
         }
 
         [Fact]
-        public void PostgreSql_statements_drop_migration_with_schema()
+        public void MySql_statements_drop_migration_with_schema()
         {
             //Arrange
             const string table = "mymigration";
@@ -168,7 +168,7 @@ namespace MinimigTests.Unit
             const string expected = "DROP";
 
             //Act
-            var s = new PostgreSqlStatements(table, schema);
+            var s = new MySqlStatements(table, schema);
 
             //Assert
             Assert.Contains(expected, s.DropMigrationsTable, StringComparison.InvariantCultureIgnoreCase);
@@ -177,20 +177,20 @@ namespace MinimigTests.Unit
         }
 
         [Fact]
-        public void PostgreSql_statements_sql_server_command_splitter()
+        public void MySql_statements_command_splitter()
         {
             //Arrange
-            const string value = "GO";
-            const string lowerValue = "go";
+            const string value = ";";
+            const string lowerValue = ";";
             const string table = "mymigration";
             const string schema = "tst";
 
             //Act
-            var s = new PostgreSqlStatements(table, schema);
+            var s = new MySqlStatements(table, schema);
 
             //Assert
-            Assert.False(s.CommandSplitter.Match(value).Success);
-            Assert.False(s.CommandSplitter.Match(lowerValue).Success);
+            Assert.True(s.CommandSplitter.Match(value).Success);
+            Assert.True(s.CommandSplitter.Match(lowerValue).Success);
         }
     }
 }
