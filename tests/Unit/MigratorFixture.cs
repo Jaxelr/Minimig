02b6d75ae9e836fc62233f2019ff9234deb5fe0b
Jaxel Rojas <jaxelr@gmail.com>
2021-12-20 15:44:34 -0400

    chore: bump version to 0.10.0

--- tests/Unit/MigratorFixture.cs
+++ tests/Unit/MigratorFixture.cs
@@ -9,7 +9,7 @@ namespace MinimigTests.Unit
         public void Migrator_get_version()
         {
             //Arrange
-            const string version = "0.9.0";
+            const string version = "0.10.0";
 
             //Act
             string result = Migrator.GetVersion();
