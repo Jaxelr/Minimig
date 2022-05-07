0de0c7e59dcb3b3288a576e9b4ca9543bf7716b3
Jaxel Rojas <jaxelr@gmail.com>
2022-05-07 09:03:51 -0400

    chore: remove the unused package

--- tests/Unit/MigratorFixture.cs
+++ tests/Unit/MigratorFixture.cs
@@ -1,22 +1,21 @@
 ﻿using Minimig;
 using Xunit;
 
-namespace MinimigTests.Unit
+namespace MinimigTests.Unit;
+
+public class MigratorFixture
 {
-    public class MigratorFixture
+    [Theory]
+    [InlineData("(local)", "master", true, "previewTable")]
+    public void Migrator_instantiation_preview(string server, string database, bool isPreview, string table)
     {
-        [Theory]
-        [InlineData("(local)", "master", true, "previewTable")]
-        public void Migrator_instantiation_preview(string server, string database, bool isPreview, string table)
-        {
-            //Arrange
-            var option = new Options() { Server = server, Database = database, Preview = isPreview, Table = table };
+        //Arrange
+        var option = new Options() { Server = server, Database = database, Preview = isPreview, Table = table };
 
-            //Act
-            using var mig = new Migrator(option);
+        //Act
+        using var mig = new Migrator(option);
 
-            //Assert
-            Assert.Empty(mig.Migrations);
-        }
+        //Assert
+        Assert.Empty(mig.Migrations);
     }
 }
--- tests/Unit/OptionsFixture.cs
+++ tests/Unit/OptionsFixture.cs
@@ -2,197 +2,196 @@
 using Minimig;
 using Xunit;
 
-namespace MinimigTests.Unit
+namespace MinimigTests.Unit;
+
+/*
+ * Some of these unit tests require that we use trusted connections which means that the sql instance cannot be a docker image.
+ */
+
+public class OptionsFixture
 {
-    /*
-     * Some of these unit tests require that we use trusted connections which means that the sql instance cannot be a docker image.
-     */
-
-    public class OptionsFixture
+    [Theory]
+    [InlineData("MyMigration")]
+    public void Get_migration_table_with_value(string inputTable)
     {
-        [Theory]
-        [InlineData("MyMigration")]
-        public void Get_migration_table_with_value(string inputTable)
-        {
-            //Arrange
-            var options = new Options() { Table = inputTable };
+        //Arrange
+        var options = new Options() { Table = inputTable };
 
-            //Act
-            string table = options.GetMigrationsTable();
+        //Act
+        string table = options.GetMigrationsTable();
 
-            //Assert
-            Assert.Equal(inputTable, table);
-        }
+        //Assert
+        Assert.Equal(inputTable, table);
+    }
 
-        [Fact]
-        public void Get_migration_table_default()
-        {
-            //Arrange
-            const string inputTable = "Migrations";
-            var options = new Options();
+    [Fact]
+    public void Get_migration_table_default()
+    {
+        //Arrange
+        const string inputTable = "Migrations";
+        var options = new Options();
 
-            //Act
-            string table = options.GetMigrationsTable();
+        //Act
+        string table = options.GetMigrationsTable();
 
-            //Assert
-            Assert.Equal(inputTable, table);
-        }
+        //Assert
+        Assert.Equal(inputTable, table);
+    }
 
-        [Theory]
-        [InlineData(@"C:\\temp\\")]
-        public void Get_migration_folder_with_value(string inputFolder)
-        {
-            //Arrange
-            var options = new Options() { Folder = inputFolder };
+    [Theory]
+    [InlineData(@"C:\\temp\\")]
+    public void Get_migration_folder_with_value(string inputFolder)
+    {
+        //Arrange
+        var options = new Options() { Folder = inputFolder };
 
-            //Act
-            string folder = options.GetFolder();
+        //Act
+        string folder = options.GetFolder();
 
-            //Assert
-            Assert.Equal(inputFolder, folder);
-        }
+        //Assert
+        Assert.Equal(inputFolder, folder);
+    }
 
-        [Fact]
-        public void Get_migration_folder_default()
-        {
-            //Arrange
-            string inputFolder = System.IO.Directory.GetCurrentDirectory();
-            var options = new Options();
+    [Fact]
+    public void Get_migration_folder_default()
+    {
+        //Arrange
+        string inputFolder = System.IO.Directory.GetCurrentDirectory();
+        var options = new Options();
 
-            //Act
-            string folder = options.GetFolder();
+        //Act
+        string folder = options.GetFolder();
 
-            //Assert
-            Assert.Equal(inputFolder, folder);
-        }
+        //Assert
+        Assert.Equal(inputFolder, folder);
+    }
 
-        [Theory]
-        [InlineData(@"C:\\temp\\")]
-        public void Get_migration_connection_string_with_value(string inputConnection)
-        {
-            //Arrange
-            var options = new Options() { Connection = inputConnection };
+    [Theory]
+    [InlineData(@"C:\\temp\\")]
+    public void Get_migration_connection_string_with_value(string inputConnection)
+    {
+        //Arrange
+        var options = new Options() { Connection = inputConnection };
 
-            //Act
-            string conn = options.GetConnectionString(DatabaseProvider.sqlserver);
+        //Act
+        string conn = options.GetConnectionString(DatabaseProvider.sqlserver);
 
-            //Assert
-            Assert.Equal(inputConnection, conn);
-        }
+        //Assert
+        Assert.Equal(inputConnection, conn);
+    }
 
-        [Theory]
-        [InlineData(".", "master")]
-        public void Get_connection_string_with_server_and_database_sqlserver(string inputServer, string inputDatabase)
-        {
-            //Arrange
-            string inputConnection = $"Persist Security Info=False;Integrated Security=true;Initial Catalog={inputDatabase};server={inputServer}";
-            var options = new Options() { Server = inputServer, Database = inputDatabase };
+    [Theory]
+    [InlineData(".", "master")]
+    public void Get_connection_string_with_server_and_database_sqlserver(string inputServer, string inputDatabase)
+    {
+        //Arrange
+        string inputConnection = $"Persist Security Info=False;Integrated Security=true;Initial Catalog={inputDatabase};server={inputServer}";
+        var options = new Options() { Server = inputServer, Database = inputDatabase };
 
-            //Act
-            string conn = options.GetConnectionString(DatabaseProvider.sqlserver);
+        //Act
+        string conn = options.GetConnectionString(DatabaseProvider.sqlserver);
 
-            //Assert
-            Assert.Equal(inputConnection, conn);
-        }
+        //Assert
+        Assert.Equal(inputConnection, conn);
+    }
 
-        [Theory]
-        [InlineData("localhost", "postgres")]
-        public void Get_connection_string_with_server_and_database_postgres(string inputServer, string inputDatabase)
-        {
-            //Arrange
-            string inputConnection = $"Server={inputServer};Port=5432;Database={inputDatabase};Integrated Security=true;";
-            var options = new Options() { Server = inputServer, Database = inputDatabase };
+    [Theory]
+    [InlineData("localhost", "postgres")]
+    public void Get_connection_string_with_server_and_database_postgres(string inputServer, string inputDatabase)
+    {
+        //Arrange
+        string inputConnection = $"Server={inputServer};Port=5432;Database={inputDatabase};Integrated Security=true;";
+        var options = new Options() { Server = inputServer, Database = inputDatabase };
 
-            //Act
-            string conn = options.GetConnectionString(DatabaseProvider.postgresql);
+        //Act
+        string conn = options.GetConnectionString(DatabaseProvider.postgresql);
 
-            //Assert
-            Assert.Equal(inputConnection, conn);
-        }
+        //Assert
+        Assert.Equal(inputConnection, conn);
+    }
 
-        [Theory]
-        [InlineData("master")]
-        public void Get_connection_string_with_database(string inputDatabase)
-        {
-            //Arrange
-            string inputConnection = $"Persist Security Info=False;Integrated Security=true;Initial Catalog={inputDatabase};server=.";
-            var options = new Options() { Database = inputDatabase };
+    [Theory]
+    [InlineData("master")]
+    public void Get_connection_string_with_database(string inputDatabase)
+    {
+        //Arrange
+        string inputConnection = $"Persist Security Info=False;Integrated Security=true;Initial Catalog={inputDatabase};server=.";
+        var options = new Options() { Database = inputDatabase };
 
-            //Act
-            string conn = options.GetConnectionString(DatabaseProvider.sqlserver);
+        //Act
+        string conn = options.GetConnectionString(DatabaseProvider.sqlserver);
 
-            //Assert
-            Assert.Equal(inputConnection, conn);
-        }
+        //Assert
+        Assert.Equal(inputConnection, conn);
+    }
 
-        [Theory]
-        [InlineData("myMigrations", "master")]
-        public void Assert_valid_migrations_table(string table, string db)
-        {
-            //Arrange
-            var options = new Options() { Table = table, Database = db };
+    [Theory]
+    [InlineData("myMigrations", "master")]
+    public void Assert_valid_migrations_table(string table, string db)
+    {
+        //Arrange
+        var options = new Options() { Table = table, Database = db };
 
-            //Act
-            var action = new Action(options.AssertValid);
-            var result = (Options) action.Target;
-            action.Invoke();
+        //Act
+        var action = new Action(options.AssertValid);
+        var result = (Options) action.Target;
+        action.Invoke();
 
-            //Assert
-            Assert.Equal(result.Table, table);
-            Assert.Equal(result.Database, db);
-            Assert.NotNull(result);
-        }
+        //Assert
+        Assert.Equal(table, result.Table);
+        Assert.Equal(db, result.Database);
+        Assert.NotNull(result);
+    }
 
-        [Fact]
-        public void Assert_valid_exception()
-        {
-            //Arrange
-            var options = new Options();
+    [Fact]
+    public void Assert_valid_exception()
+    {
+        //Arrange
+        var options = new Options();
 
-            //Act
-            var action = new Action(options.AssertValid);
+        //Act
+        var action = new Action(options.AssertValid);
 
-            //Assert
-            Assert.Throws<Exception>(action);
-        }
+        //Assert
+        Assert.Throws<Exception>(action);
+    }
 
-        [Fact]
-        public void Assert_valid_exception_invalid_path()
-        {
-            //Arrange
-            var options = new Options() { Folder = "C:\\randommissingpath\\" };
+    [Fact]
+    public void Assert_valid_exception_invalid_path()
+    {
+        //Arrange
+        var options = new Options() { Folder = "C:\\randommissingpath\\" };
 
-            //Act
-            var action = new Action(options.AssertValid);
+        //Act
+        var action = new Action(options.AssertValid);
 
-            //Assert
-            Assert.Throws<Exception>(action);
-        }
+        //Assert
+        Assert.Throws<Exception>(action);
+    }
 
-        [Fact]
-        public void Assert_valid_exception_migrations_table()
-        {
-            //Arrange
-            var options = new Options() { Table = "$Inv@lid N@me", Database = "master" };
+    [Fact]
+    public void Assert_valid_exception_migrations_table()
+    {
+        //Arrange
+        var options = new Options() { Table = "$Inv@lid N@me", Database = "master" };
 
-            //Act
-            var action = new Action(options.AssertValid);
+        //Act
+        var action = new Action(options.AssertValid);
 
-            //Assert
-            Assert.Throws<Exception>(action);
-        }
+        //Assert
+        Assert.Throws<Exception>(action);
+    }
 
-        [Fact]
-        public void Assert_valid_exception_migrations_table_empty()
-        {
-            //Arrange
-            var options = new Options() { Table = "" };
+    [Fact]
+    public void Assert_valid_exception_migrations_table_empty()
+    {
+        //Arrange
+        var options = new Options() { Table = "" };
 
-            //Act
-            var action = new Action(options.AssertValid);
+        //Act
+        var action = new Action(options.AssertValid);
 
-            //Assert
-            Assert.Throws<Exception>(action);
-        }
+        //Assert
+        Assert.Throws<Exception>(action);
     }
 }
