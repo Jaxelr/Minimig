3cfc1d12f588e80c9e27e4e6c71b0bf6d245a6a9
Jaxel Rojas <jaxelr@gmail.com>
2024-08-18 10:52:47 -0400

    refactor: added default constructors to exceptions

--- src/MinimigLib/Exceptions/MigrationChangedException.cs
+++ src/MinimigLib/Exceptions/MigrationChangedException.cs
@@ -7,4 +7,16 @@ public class MigrationChangedException : Exception
     internal MigrationChangedException(Migration migration) : base($"{migration.Filename} has been modified since it was run. Use --force to re-run it.")
     {
     }
+
+    public MigrationChangedException() : base()
+    {
+    }
+
+    public MigrationChangedException(string message) : base(message)
+    {
+    }
+
+    public MigrationChangedException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
 }
