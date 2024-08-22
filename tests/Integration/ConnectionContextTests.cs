763fcafe021a0aaca8c743cfc73c78f8411d713f
Jaxel Rojas <jaxelr@gmail.com>
2024-08-22 08:40:37 -0400

    fix: appveyor postgres password for image database

--- tests/Integration/ConnectionContextTests.cs
+++ tests/Integration/ConnectionContextTests.cs
@@ -22,7 +22,7 @@ public class ConnectionContextTests
 
         string postgresConnEnv = Environment.GetEnvironmentVariable("Postgres_Connection");
         if (string.IsNullOrEmpty(postgresConnEnv))
-            postgresConnEnv = "Server=localhost;Port=5432;Database=postgres;Username=postgres;Password=Password12;";
+            postgresConnEnv = "Server=localhost;Port=5432;Database=postgres;Username=postgres;Password=Password12!;";
 
         string mysqlConnEnv = Environment.GetEnvironmentVariable("MySql_Connection");
         if (string.IsNullOrEmpty(mysqlConnEnv))
